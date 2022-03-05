3c33b8fa25ae430811382e301452d555a0f4dd60
Defne Eroglu <defne@appsamurai.com>
2022-03-05 11:30:15 +0300

    update app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import Button from './components/Button';
+import { buttons } from './mock/buttons';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {buttons.map((button, index) => (
+        <Button
+          key={`${button.text}_${index}`}
+          width={button.width}
+          originalLongText={button.text}
+        />
+      ))}
     </div>
   );
 }
