b6677b4d0214c905a8af77bf4a1535092150abfe
Defne Eroglu <defne@appsamurai.com>
2022-03-05 11:30:02 +0300

    add mock dynamic data

---
+++ src/mock/buttons.js
@@ -0,0 +1,14 @@
+export const buttons = [
+  {
+    text: 'longer text button',
+    width: '128px',
+  },
+  {
+    text: 'clickking me!',
+    width: '98px',
+  },
+  {
+    text: 'Create new context',
+    width: '170px',
+  },
+];
