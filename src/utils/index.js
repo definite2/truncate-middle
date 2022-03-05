392840c4a8aa74634a037781a111c4a9010fa5a3
Defne Eroglu <defne@appsamurai.com>
2022-03-05 11:29:16 +0300

    add util to truncate from middle

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+export const truncateFromMiddle = (fullStr, strLen, separator = '...') => {
+  if (fullStr.length <= strLen) return fullStr;
+  const sepLen = separator.length;
+  const charsToShow = strLen - sepLen;
+  const frontChars = Math.ceil(charsToShow / 2);
+  const backChars = Math.floor(charsToShow / 2);
+  return (
+    fullStr.substr(0, frontChars) +
+    separator +
+    fullStr.substr(fullStr.length - backChars)
+  );
+};
