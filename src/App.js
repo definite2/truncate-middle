ba358b71865d1348ec1fc8d582d143d26f2459e7
Defne Eroglu <defne@appsamurai.com>
2022-03-06 11:29:21 +0300

    update styles, add title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h2>Truncating Button Text From Middle</h2>
       {buttons.map((button, index) => (
         <Button
           key={`${button.text}_${index}`}
