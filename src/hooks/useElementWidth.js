917f8b6569bea071db0050230b42966759fa53ff
Defne Eroglu <defne@appsamurai.com>
2022-04-16 15:09:35 +0300

    update elemnt width calculation

--- src/hooks/useElementWidth.js
+++ src/hooks/useElementWidth.js
@@ -1,51 +1,38 @@
-import { useCallback, useEffect, useState } from 'react';
-
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useCallback, useLayoutEffect, useState } from 'react';
 /**
  * Returns current width of specified element.
  *
  * @param {Ref} ref element to use in width calculation
  */
 const useElementWidth = (ref) => {
+  const [width, setWidth] = useState(0);
+
   const getWidth = useCallback(() => {
     return (
       ref?.current?.getBoundingClientRect().width *
         (window.visualViewport?.scale || 1) || 0
     );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ref?.current]);
-
-  const [width, setWidth] = useState(0);
 
   const elObserver = new ResizeObserver((entries) => {
-    window.requestAnimationFrame(() => {
-      if (!ref?.current) return;
-
-      if (entries?.[0]?.contentBoxSize?.[0]) {
-        setWidth(entries[0].contentBoxSize[0].inlineSize);
-      } else if (entries?.contentBoxSize) {
-        setWidth(entries[0].contentBoxSize.inlineSize);
-      } else {
-        setWidth(entries[0].contentRect.width);
+    for (const entry of entries) {
+      if (entry.contentBoxSize) {
+        setWidth(entry.contentRect.width);
       }
-    });
+    }
   });
-
-  useEffect(() => {
+  useLayoutEffect(() => {
     setWidth(getWidth());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ref?.current]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (!ref?.current) return;
     elObserver.observe(ref?.current);
-    // eslint-disable-next-line consistent-return
     return () => {
-      if (!ref?.current) return;
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      elObserver.unobserve(ref?.current);
+      elObserver.disconnect();
     };
   });
-
   return width;
 };
 
